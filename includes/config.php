8c2419e2cf9d4134053fd971e1e974004471de8b
Amir Azimi <hazz.azimi@gmail.com>
2015-11-14 14:41:05 +0000

    Config Change

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@
  * DB_NAME   : your database name
  */
 defined('DB_SERVER') ? NULL : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? NULL : define("DB_USER"  , "root");
-defined('DB_PASS')   ? NULL : define("DB_PASS"  , "root");
+defined('DB_USER')   ? NULL : define("DB_USER"  , "b26385d5fe1efb");
+defined('DB_PASS')   ? NULL : define("DB_PASS"  , "f7049e89");
 defined('DB_NAME')   ? NULL : define("DB_NAME"  , "parsclick");
 ?>
