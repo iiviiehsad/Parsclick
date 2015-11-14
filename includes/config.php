a79da6fc2da3f38cb48e138fcec0f89fbe62eebf
Amir Azimi <hazz.azimi@gmail.com>
2015-11-14 16:23:45 +0000

    PostgreSQL added

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,26 @@
 <?php
 /**
  * Change your database configuration here
- * DB_SERVER : your server name
- * DB_USER   : your database username
- * DB_PASS   : your database password
- * DB_NAME   : your database name
+ * DB_SERVER : your MySQL server name
+ * DB_USER   : your MySQL database username
+ * DB_PASS   : your MySQL database password
+ * DB_NAME   : your MySQL database name
  */
 defined('DB_SERVER') ? NULL : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? NULL : define("DB_USER"  , "b26385d5fe1efb");
-defined('DB_PASS')   ? NULL : define("DB_PASS"  , "f7049e89");
+defined('DB_USER')   ? NULL : define("DB_USER"  , "root");
+defined('DB_PASS')   ? NULL : define("DB_PASS"  , "root");
 defined('DB_NAME')   ? NULL : define("DB_NAME"  , "parsclick");
+/**
+ * Change your database configuration here
+ * PG_SERVER : your PostgreSQL server name
+ * PG_PORT   : your PostgreSQL port number
+ * PG_USER   : your PostgreSQL database username
+ * PG_PASS   : your PostgreSQL database password
+ * PG_NAME   : your PostgreSQL database name
+ */
+defined('PG_SERVER') ? NULL : define("PG_SERVER", "localhost");
+defined('PG_PORT')   ? NULL : define("PG_PORT"  , "5432");
+defined('PG_USER')   ? NULL : define("PG_USER"  , "hassan");
+defined('PG_PASS')   ? NULL : define("PG_PASS"  , "azimi");
+defined('PG_NAME')   ? NULL : define("PG_NAME"  , "Parsclick");
 ?>
--- includes/database.php
+++ includes/database.php
@@ -26,11 +26,11 @@ class MySQLDatabase {
 	public function open_connection() {
 		$this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 		if(!$this->connection) {
-			die("Databse connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . " )");
+			die("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . " )");
 		} else {
 			$db_select = mysqli_select_db($this->connection, DB_NAME);
 			if(!$db_select) {
-				die("Databse selection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . " )");
+				die("Database selection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . " )");
 			}
 		}
 	}
@@ -121,4 +121,125 @@ class MySQLDatabase {
 $database = new MySQLDatabase();
 // Reference the $database to $db so we can use $db or $database anyone we want
 $db =& $database;
+
+/**
+ * Class PostgresSQLDatabase is created to use PostgresSQL database and has only functions related to PostgresSQL
+ */
+class PostgresSQLDatabase {
+
+	private $connection;
+	public  $last_query;
+	private $magic_quotes_active;
+	private $real_escape_string_exists;
+
+	/**
+	 * Constructor will open the connection automatically whe the class is called or instantiated.
+	 */
+	function __construct() {
+		$this->open_connection();
+		$this->magic_quotes_active       = get_magic_quotes_gpc();
+		$this->real_escape_string_exists = function_exists("pg_escape_string");
+	}
+
+	/**
+	 * This will open the connection
+	 */
+	public function open_connection() {
+		$this->connection =
+				pg_connect("host=" . PG_SERVER . " port=" . PG_PORT . " dbname=" . PG_NAME . " user=" . PG_USER . " password=" .
+				           PG_PASS);
+		if(!$this->connection) {
+			die("Database connection failed!");
+		}
+	}
+
+	/**
+	 * This will close the connection
+	 */
+	public function close_connection() {
+		if(isset($this->connection)) {
+			pg_close($this->connection);
+			unset($this->connection);
+		}
+	}
+
+	/**
+	 * @param $sql string will get the SQL query
+	 * @return bool|mysqli_result will return the result if the SQL query is OK
+	 */
+	public function query($sql) {
+		$this->last_query = $sql;
+		$result           = pg_query($this->connection, $sql);
+		$this->confirm_query($result);
+		return $result;
+	}
+
+	/**
+	 * @param $value string will get the value and prepare it to put in MySQL
+	 * @return string will return MySQL input got from somewhere else
+	 */
+	public function escape_value($value) {
+		if($this->real_escape_string_exists) { // PHP v4.3.0+
+			// undo any magic quote effects so mysqli_real_escape_string can do the work
+			if($this->magic_quotes_active) {
+				$value = stripslashes($value);
+			}
+			$value = pg_escape_string($this->connection, $value); // PHP v5.0+
+		} else { // before PHP v4.3.0
+			// if magic quotes aren't already on then add slashes manually
+			if(!$this->magic_quotes_active) {
+				$value = addslashes($value);
+			} // if magic quotes are active, then the slashes already exist
+		}
+		return $value;
+	}
+
+	/**
+	 * @param $result_set
+	 * @return array|null associative array
+	 */
+	public function fetch_assoc($result_set) {
+		return pg_fetch_assoc($result_set);
+	}
+
+	/**
+	 * @param $result_set
+	 * @return int number of rows
+	 */
+	public function num_rows($result_set) {
+		return pg_num_rows($result_set);
+	}
+
+	/**
+	 * @return int|string gets the last id inserted over the current db connection
+	 */
+	public function insert_id() {
+		return pg_last_oid($this->last_query);
+	}
+
+	/**
+	 * @return int how many rows were affected by the last query
+	 */
+	public function affected_rows() {
+		return pg_affected_rows($this->connection);
+	}
+
+	/**
+	 * @param $result boolean checks if the query is OK
+	 */
+	private function confirm_query($result) {
+		if(!$result) {
+			$output = "Database query failed!";;
+			$output .= "Last SQL Query: " . $this->last_query;
+			die($output);
+		}
+	}
+}
+
+//$database = new MySQLDatabase();
+$database = new PostgresSQLDatabase();
+
+// Reference the $database to $db so we can use $db or $database anyone we want
+$db =& $database;
+?>
 ?>
