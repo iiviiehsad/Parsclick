f0efaed29be9bde469f501d710749a2384d8b61a
Amir Azimi <hazz.azimi@gmail.com>
2015-11-17 23:29:01 +0000

    Encryption and Decryption functions are added

--- includes/functions.php
+++ includes/functions.php
@@ -200,6 +200,60 @@ function log_action($action, $message = "") {
 }
 
 /**
+ * @param $salt string gets the salt to add to the @param $string
+ * @param $string $string string gets the text
+ * @return string encrypts the string
+ */
+function encrypt_string($salt, $string) {
+	// Configuration (must match decryption)
+	$cipher_type = MCRYPT_RIJNDAEL_256;
+	$cipher_mode = MCRYPT_MODE_CBC;
+	// Using initialization vector adds more security
+	$iv_size = mcrypt_get_iv_size($cipher_type, $cipher_mode);
+	$iv      = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$encrypted_string = mcrypt_encrypt($cipher_type, $salt, $string, $cipher_mode, $iv);
+	// Return initialization vector + encrypted string
+	// We'll need the $iv when decoding.
+	return $iv . $encrypted_string;
+}
+
+/**
+ * @param $salt string gets the salt to add to the @param $string
+ * @param $iv_with_string string initialization vector
+ * @return string decrypts the string
+ */
+function decrypt_string($salt, $iv_with_string) {
+	// Configuration (must match encryption)
+	$cipher_type = MCRYPT_RIJNDAEL_256;
+	$cipher_mode = MCRYPT_MODE_CBC;
+	// Extract the initialization vector from the encrypted string.
+	// The $iv comes before encrypted string and has fixed size.
+	$iv_size          = mcrypt_get_iv_size($cipher_type, $cipher_mode);
+	$iv               = substr($iv_with_string, 0, $iv_size);
+	$encrypted_string = substr($iv_with_string, $iv_size);
+	$string = mcrypt_decrypt($cipher_type, $salt, $encrypted_string, $cipher_mode, $iv);
+	return $string;
+}
+
+/**
+ * @param $salt string gets the salt to add to the @param $string
+ * @param $string string gets the text
+ * @return string encode after encryption to ensure encrypted characters are savable
+ */
+function encrypt_string_and_encode($salt, $string) {
+	return base64_encode(encrypt_string($salt, $string));
+}
+
+/**
+ * @param $salt string gets the salt to add it to the @param $string
+ * @param $string string gets the text
+ * @return string and decodes before decryption
+ */
+function decrypt_string_and_decode($salt, $string) {
+	return decrypt_string($salt, base64_decode($string));
+}
+
+/**
  * Function for super admins to show the subjects and articles
  * @param $subject_array array gets the subject ID form URL and return it as an array
  * @param $article_array array gets the article ID form URL and return it as an array
