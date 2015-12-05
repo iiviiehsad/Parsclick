5237bff6a1763a793ac3747cd413f31970510413
Amir Azimi <hazz.azimi@gmail.com>
2015-12-05 15:34:54 +0000

    FAQ is so much improved!

---
+++ public/_/components/php/aside-twitter-embed.php
@@ -0,0 +1,9 @@
+<br>
+<blockquote class="twitter-tweet" lang="fa-IR">
+	<p lang="fa" dir="rtl">Great speech today in <a href="https://twitter.com/hashtag/interop?src=hash">#interop</a> from
+		<a href="https://twitter.com/hashtag/Pluralsight?src=hash">#Pluralsight</a> which is one of the best online tutorial
+	                       makers in the market <a href="http://t.co/FANB7rOvZi">pic.twitter.com/FANB7rOvZi</a></p>&mdash;
+	Amir Azimi (@AmirHassanAzimi) <a href="https://twitter.com/AmirHassanAzimi/status/610873676792209408">
+		June 16, 2015</a>
+</blockquote>
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
--- public/faq.php
+++ public/faq.php
@@ -11,5 +11,6 @@ $title    = "پارس کلیک - سوالات و جواب ها";
 	<section class="sidebar col-sm-12 col-md-4 col-lg-4">
 		<?php include("_/components/php/aside-register.php"); ?>
 		<?php include("_/components/php/aside-twitter.php"); ?>
+		<?php include("_/components/php/aside-twitter-embed.php"); ?>
 	</section><!-- sidebar -->
 <?php include_layout_template("footer.php"); ?>
