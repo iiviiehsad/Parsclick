bcbb339cedf9a085da47c0e966866d55f096c302
Amir Azimi <hazz.azimi@gmail.com>
2015-12-07 15:07:19 +0000

    member course and article info fix

--- public/_/components/php/member_article_info.php
+++ public/_/components/php/member_article_info.php
@@ -3,30 +3,30 @@
 <br/>
 <section class="row">
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInRight" data-wow-duration="1s">
-		<i class="center fa fa-newspaper-o fa-fw fa-3x" style="color: #56b98a;"></i>
+		<div class="center"><i class="fa fa-newspaper-o fa-fw fa-3x" style="color: #56b98a;"></i></div>
 		<p class="center lead">اخبار</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInDown" data-wow-duration="1s" data-wow-delay=".25s">
-		<i class="center fa fa-keyboard-o fa-fw fa-3x" style="color: #b973ac;"></i>
+		<div class="center"><i class="fa fa-keyboard-o fa-fw fa-3x" style="color: #b973ac;"></i></div>
 		<p class="center lead">نظرات شخصی</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-		<i class="center fa fa-sitemap fa-fw fa-3x" style="color: #5e60b9;"></i>
+		<div class="center"><i class="center fa fa-sitemap fa-fw fa-3x" style="color: #5e60b9;"></i></div>
 		<p class="center lead">بحث های داغ</p>
 	</div>
 </section>
 <br/>
 <section class="row">
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInRight" data-wow-duration="1s" data-wow-delay=".75s">
-		<i class="center fa fa-desktop fa-fw fa-3x" style="color: #b97741;"></i>
+		<div class="center"><i class="fa fa-desktop fa-fw fa-3x" style="color: #b97741;"></i></div>
 		<p class="center lead">آموزش</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
-		<i class="center fa fa-user-secret fa-fw fa-3x" style="color: #8fb945;"></i>
+		<div class="center"><i class="fa fa-user-secret fa-fw fa-3x" style="color: #8fb945;"></i></div>
 		<p class="center lead">ترفند</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1.25s">
-		<i class="center fa fa-question fa-fw fa-3x" style="color: #cb4d65;"></i>
+		<div class="center"><i class="fa fa-question fa-fw fa-3x" style="color: #cb4d65;"></i></div>
 		<p class="center lead">جواب های سوالات</p>
 	</div>
 </section>
--- public/_/components/php/member_course_info.php
+++ public/_/components/php/member_course_info.php
@@ -3,25 +3,27 @@
 <br/><br/><br/>
 <section class="row">
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInRight" data-wow-duration="1s">
-		<i class="center fa fa-code fa-3x" style="color: #b90051;"></i>
+		<div class="center"><i class="fa fa-code fa-3x" style="color: #b90051;"></i></div>
 		<p class="center lead">برنامه نویسی</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInDown" data-wow-duration="1s" data-wow-delay=".25s">
 		<div class="center">
-			<i class="fa fa-html5 fa-3x" style="color: #e54d26;"></i>
-			<i class="fa fa-css3 fa-3x fa-fw" style="color: #006eb9;"></i>
+			<div class="center">
+				<i class="fa fa-html5 fa-3x" style="color: #e54d26;"></i>
+				<i class="fa fa-css3 fa-3x fa-fw" style="color: #006eb9;"></i>
+			</div>
 		</div>
 		<p class="center lead">وب</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-		<i class="center fa fa-picture-o fa-3x fa-fw" style="color: #e18824;"></i>
+		<div class="center"><i class="fa fa-picture-o fa-3x fa-fw" style="color: #e18824;"></i></div>
 		<p class="center lead">طراحی</p>
 	</div>
 </section>
 <br/><br/>
 <section class="row">
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInRight" data-wow-duration="1s" data-wow-delay=".75s">
-		<i class="center fa fa-laptop fa-3x fa-fw" style="color: #af48cd;"></i>
+		<div class="center"><i class="fa fa-laptop fa-3x fa-fw" style="color: #af48cd;"></i></div>
 		<p class="center lead">علم فنی کامپیوتر</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">
@@ -33,7 +35,7 @@
 		<p class="center lead">سیستم مدیریت محتوا</p>
 	</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1.25s">
-		<i class="center fa fa-server fa-3x" style="color: #2fb969;"></i>
+		<div class="center"><i class="fa fa-server fa-3x" style="color: #2fb969;"></i></div>
 		<p class="center lead">اینترنت</p>
 	</div>
 </section>
