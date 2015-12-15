1797957da920d48c8d0beb74b6b6786211c8a34f
Amir Azimi <hazz.azimi@gmail.com>
2015-12-15 01:00:02 +0000

    ip_info function is added
    now I can target people's country by their IP address
     if( ip_info("Visitor", "Country Code") == "IR") {
        echo "You are from Iran!";
     }

--- includes/functions.php
+++ includes/functions.php
@@ -117,6 +117,95 @@ function truncate($string, $length, $dots = " (برای ادامه کلیک کن
 	return (strlen($string) > $length) ? substr($string, 0, $length - strlen($dots)) . $dots : $string;
 }
 
+/**
+ * echo ip_info("173.252.110.27", "Country");
+ * echo ip_info("173.252.110.27", "Country Code");
+ * echo ip_info("173.252.110.27", "State");
+ * echo ip_info("173.252.110.27", "City");
+ * echo ip_info("173.252.110.27", "Address");
+ * ------------------------------------------
+ * echo ip_info("Visitor", "Country");
+ * echo ip_info("Visitor", "Country Code");
+ * echo ip_info("Visitor", "State");
+ * echo ip_info("Visitor", "City");
+ * echo ip_info("Visitor", "Address");
+ *
+ * @param null   $ip          gets the IP address
+ * @param string $purpose     gets Country, Country Code, State, City or Address
+ * @param bool   $deep_detect TRUE is user is using proxy and FALSE otherwise
+ *
+ * @return array|null|string of location details
+ */
+function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE)
+{
+	$output = NULL;
+	if(filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+		$ip = $_SERVER["REMOTE_ADDR"];
+		if($deep_detect) {
+			if(filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP)) {
+				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			}
+			if(filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP)) {
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			}
+		}
+	}
+	$purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+	$support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+	$continents = array(
+			"AF" => "Africa",
+			"AN" => "Antarctica",
+			"AS" => "Asia",
+			"EU" => "Europe",
+			"OC" => "Australia (Oceania)",
+			"NA" => "North America",
+			"SA" => "South America"
+	);
+	if(filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+		$ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+		if(@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+			switch($purpose) {
+				case "location":
+					$output = array(
+							"city"           => @$ipdat->geoplugin_city,
+							"state"          => @$ipdat->geoplugin_regionName,
+							"country"        => @$ipdat->geoplugin_countryName,
+							"country_code"   => @$ipdat->geoplugin_countryCode,
+							"continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+							"continent_code" => @$ipdat->geoplugin_continentCode
+					);
+					break;
+				case "address":
+					$address = array($ipdat->geoplugin_countryName);
+					if(@strlen($ipdat->geoplugin_regionName) >= 1) {
+						$address[] = $ipdat->geoplugin_regionName;
+					}
+					if(@strlen($ipdat->geoplugin_city) >= 1) {
+						$address[] = $ipdat->geoplugin_city;
+					}
+					$output = implode(", ", array_reverse($address));
+					break;
+				case "city":
+					$output = @$ipdat->geoplugin_city;
+					break;
+				case "state":
+					$output = @$ipdat->geoplugin_regionName;
+					break;
+				case "region":
+					$output = @$ipdat->geoplugin_regionName;
+					break;
+				case "country":
+					$output = @$ipdat->geoplugin_countryName;
+					break;
+				case "countrycode":
+					$output = @$ipdat->geoplugin_countryCode;
+					break;
+			}
+		}
+	}
+	return $output;
+}
+
 /******************************************************************************************************/
 /*                                    SECURITY FUNCTIONS                                              */
 /******************************************************************************************************/
