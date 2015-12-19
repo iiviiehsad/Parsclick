705e91a0c5567a1118a72e832c0c978948d4660c
Amir Azimi <hazz.azimi@gmail.com>
2015-12-19 16:12:58 +0000

    post_code and phone is removed from member_search

--- public/admin/member_search.php
+++ public/admin/member_search.php
@@ -1,8 +1,8 @@
 <?php require_once("../../includes/initialize.php");
 $session->confirm_admin_logged_in();
-$filename = basename(__FILE__);
+$filename     = basename(__FILE__);
 $search_query = trim($_GET["q"]);
-if(isset($search_query) && !empty($search_query)) {
+if(isset($search_query) && ! empty($search_query)) {
 	$member_set = Member::search($search_query);
 } else { // this is a $_GET request
 	$session->message("چیزی جستجو نکردید.");
@@ -14,7 +14,7 @@ echo output_message($message);
 ?>
 	<section class="main">
 		<article>
-			<?php if(!empty($member_set)) { ?>
+			<?php if(! empty($member_set)) { ?>
 				<h2><i class="fa fa-search"></i> نتیجه جستجوی اعضا </h2>
 				<div class="table-responsive">
 					<table class="table">
@@ -25,7 +25,6 @@ echo output_message($message);
 							<th>نام خانوادگی</th>
 							<th>جنس</th>
 							<th>آدرس</th>
-							<th>تلفن</th>
 							<th>ایمیل</th>
 							<th colspan="2">عملیات</th>
 						</tr>
@@ -37,10 +36,10 @@ echo output_message($message);
 								<td><?php echo htmlentities(ucfirst(strtolower($member->first_name))); ?></td>
 								<td><?php echo htmlentities(ucfirst(strtolower($member->last_name))); ?></td>
 								<td><?php echo htmlentities($member->gender); ?></td>
-								<td><?php echo htmlentities(ucwords(strtolower($member->address))); ?> <br/>
+								<td>
+									<?php echo htmlentities(ucwords(strtolower($member->address))); ?> <br/>
 									<?php echo htmlentities(ucwords(strtolower($member->city))); ?> <br/>
-									<?php echo htmlentities(strtoupper($member->post_code)); ?></td>
-								<td><?php echo htmlentities($member->phone); ?></td>
+								</td>
 								<td class="arial"><?php echo htmlentities(strtolower($member->email)); ?></td>
 								<td>
 									<a class="btn btn-small btn-primary arial" href="edit_member.php?id=<?php echo urlencode($member->id); ?>" title="Edit"><span class="glyphicon glyphicon-edit"></span></a>
