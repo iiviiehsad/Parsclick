39d05ecf5601df7b1ebf9435d470f6e4f6c5da92
Amir Azimi <hazz.azimi@gmail.com>
2015-12-19 22:46:05 +0000

    JS doc block added

--- public/_/components/js/_myscript.js
+++ public/_/components/js/_myscript.js
@@ -44,18 +44,10 @@ $(function() {
 	// INPUT FILE STYLE
 	var wrapper = $('<label/>').css({height : 0, width : 0, 'overflow' : 'hidden'});
 	$(':file').wrap(wrapper);
-	//var fileInput = $(':file').wrap(wrapper);
 
-	//fileInput.change(function() {
-	//	$this = $(this);
-	//	$('#file').text($this.val());
-	//});
-	//
-	//$('#file').click(function() {
-	//	fileInput.click();
-	//}).show();
-
-	// ADDING COURSE TO PLAYLIST USING AJAX ----------------------------------------------------------------------------------
+	/**
+	 * Adding course to playlist using AJAX
+	 */
 	$('form.addtoplaylist').on('submit', function() {
 		var that = $(this),
 		    url  = that.attr('action'),
@@ -80,7 +72,9 @@ $(function() {
 		return false;
 	});
 
-	// REMOVING COURSE FROM PLAYLIST USING AJAX --------------------------------------------------------------------------
+	/**
+	 * Removing course from playlist using AJAX
+	 */
 	$('form.removefromplaylist').on('submit', function() {
 		var that = $(this),
 		    url  = that.attr('action'),
@@ -105,7 +99,9 @@ $(function() {
 		return false;
 	});
 
-	// ADDING COMMENTS AJAX ----------------------------------------------------------------------------------------------
+	/**
+	 * Adding AJAX for comment form
+	 */
 	$('form.submit-comment').on('submit', function() {
 		var that = $(this),
 		    url  = that.attr('action'),
@@ -129,46 +125,19 @@ $(function() {
 		return false;
 	});
 
-	// FUNCTION FOR MEMBER'S VIDEO ---------------------------------------------------------------------------------------
-	//var videos = document.querySelectorAll(".videoThumbnail");
-	//for(var i = 0; i < videos.length; i++) {
-	//	videos[i].addEventListener('click', clickHandler, false);
-	//}
-	//function clickHandler(e) {
-	//	var mainVideo = document.getElementById("mainVideo");
-	//	if(!isChrome || !isSafari) {
-	//		mainVideo.src = e.target.currentSrc;
-	//	} else {
-	//		mainVideo.src = e.srcElement.currentSrc;
-	//	}
-	//}
+	/**
+	 * Perform AJAX for button when uploading file
+	 */
+	$('form.fileForm').on('submit', function() {
+		var $btn = $('#fileSubmit').button('loading');
+		setTimeout(function() {
+			$btn.button('reset');
+		}, 86400000); //1000*60*60*24*1 (1 day)
+	});
 
-	//FINDING THE BROWSERS NAME ------------------------------------------------------------------------------------------
-	//var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-	//var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-	//
-	////TO PLAY OR PAUSE VIDEO MANUALLY BY CLICKING ON THE VIDEO ---------------------------------------------------------
-	//if(isChrome || isSafari) {
-	//	var video = document.getElementById('mainVideo');
-	//	if(video) {
-	//		video.addEventListener(
-	//				'play',
-	//				function () {
-	//					video.play();
-	//				},
-	//				false);
-	//
-	//		video.onclick = function () {
-	//			if(video.paused) {
-	//				video.play();
-	//			} else {
-	//				video.pause();
-	//			}
-	//		};
-	//	}
-	//}
-
-	//SMOOTH SCROLLING ---------------------------------------------------------------------------------------------------
+	/**
+	 * Function to perform smooth scrolling
+	 */
 	//$('a[href*=#]:not([href=#myCarousel], [href=#comments])').click(function() {
 	//	if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 	//		var target = $(this.hash);
@@ -180,39 +149,22 @@ $(function() {
 	//	}
 	//});
 
-	// AJAX FOR UPLOAD VIDEO BUTTON
-	$('form.uploadForm').on('submit', function() {
-		var $btn = $('#upladSubmit').button('loading');
-		setTimeout(function() {
-			$btn.button('reset');
-		}, 86400000); //1000*60*60*24*1 (1 day)
-	});
-
-	// AJAX FOR UPLOAD FILE BUTTON
-	$('form.fileForm').on('submit', function() {
-		var $btn = $('#fileSubmit').button('loading');
-		setTimeout(function() {
-			$btn.button('reset');
-		}, 86400000); //1000*60*60*24*1 (1 day)
-	});
-
 }); //jQuery IS LOADED -----------------------------------------------------------------------------------------------
 
-//function videoPlayer(url) {
-//	var x = screen.width / 2 - 700 / 2;
-//	var y = screen.height / 2 - 450 / 2;
-//	window.open(url.href, 'sharegplus', 'width=640, height=360,left=' + x + ',top=' + y + 'toolbar=no,scrollbars=no,location=0,resizable=yes');
-//}
-
+/**
+ * Function opens the new window without address bar
+ * @param url gets the URL
+ */
 function shareButton(url) {
 	var x = screen.width / 2 - 700 / 2;
 	var y = screen.height / 2 - 450 / 2;
 	window.open(url.href, 'sharegplus', 'width=640, height=360,left=' + x + ',top=' + y + 'toolbar=no,scrollbars=no,location=0,resizable=yes');
 }
 
-//var bgvideo = document.getElementById('bgvideo');
-
-//FUNCTIONS FOR LIVE INPUT VALIDATION ----------------------------------------------------------------------------------
+/**
+ * Variables to get the registration fields
+ * @type {Element}
+ */
 var username      = document.getElementById('username');
 var pass1         = document.getElementById('password');
 var pass2         = document.getElementById('confirm_pass');
@@ -226,6 +178,10 @@ var mBadColor     = "#B94A48";
 var usernameregex = /[^a-zA-Z0-9_.]/;
 var passregex     = /[^A-Za-z0-9]/;
 
+/**
+ * Function to check the username
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checkUser() {
 	if(usernameregex.test(username.value)) {
 		username.style.backgroundColor = badColor;
@@ -249,6 +205,10 @@ function checkUser() {
 		return true;
 	}
 }
+/**
+ * Function to check the first name
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checkfirstname() {
 	if(firstname.value == '' || firstname.value == null || firstname.value == ' ') {
 		firstname.style.backgroundColor = badColor;
@@ -262,6 +222,10 @@ function checkfirstname() {
 		return true;
 	}
 }
+/**
+ * Function to check the last name
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checklastname() {
 	if(lastname.value == '' || lastname.value == null || lastname.value == ' ') {
 		lastname.style.backgroundColor = badColor;
@@ -275,6 +239,10 @@ function checklastname() {
 		return true;
 	}
 }
+/**
+ * Function to check the password
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checkPass() {
 	if(pass1.value.length < 6) {
 		pass1.style.backgroundColor = badColor;
@@ -293,6 +261,10 @@ function checkPass() {
 		return true;
 	}
 }
+/**
+ * Function to check the password confirmation
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checkConfirmPass() {
 	if(pass2.value.length < 6) {
 		pass2.style.backgroundColor = badColor;
@@ -316,6 +288,10 @@ function checkConfirmPass() {
 		return true;
 	}
 }
+/**
+ * Function to check the email
+ * @returns {boolean} TRUE if validation passes and FALSE otherwise
+ */
 function checkEmail() {
 	var regExp = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i;
 	if(regExp.test(email.value) == false) {
@@ -331,11 +307,16 @@ function checkEmail() {
 	}
 }
 
-//WOW ------------------------------------------------------------------------------------------------------------------
+/**
+ * Initializing WOW animation:
+ * WOW is used when user reaches the specific height,
+ * WOW used animate.css file in order to work
+ */
 wow = new WOW({animateClass : 'animated', offset : 100});
 wow.init();
-//data-wow-delay="2s"
-//data-wow-offset="300"
-//data-wow-duration="4s"
-//data-wow-iteration="infinite"
-//----------------------------------------------------------------------------------------------------------------------
+/**
+ * data-wow-delay="2s"
+ * data-wow-offset="300"
+ * data-wow-duration="4s"
+ * data-wow-iteration="infinite"
+ */
