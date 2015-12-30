629994a81e792ab4018d749386dcddbf8db55015
Amir Azimi <hazz.azimi@gmail.com>
2015-12-30 02:21:47 +0000

    member-article-info changed

--- public_html/_/components/php/member_article_info.php
+++ public_html/_/components/php/member_article_info.php
@@ -1,14 +1,14 @@
-<h2>به قسمت مقالات خوش آمدید.</h2><br>
-<section>
+<h2>به قسمت مقالات خوش آمدید.</h2>
+<section class="visible-lg">
 	<h4>تعداد نویسندگان: <span class="badge arial"><?php echo count(Author::find_active_authors()); ?></span></h4>
 	<?php $authors = Author::find_active_authors(); ?>
 	<?php foreach($authors as $author) { ?>
 		<div class="col-sm-12 col-md-3 col-lg-3">
 			<div class="thumbnail">
 				<?php if( ! empty($author->photo)) { ?>
-					<img class="img-circle" width="70" alt="<?php echo $author->full_name(); ?>" src="data:image/jpeg;base64,<?php echo base64_encode($author->photo); ?>"/>
+					<img width="100%" alt="<?php echo $author->full_name(); ?>" src="data:image/jpeg;base64,<?php echo base64_encode($author->photo); ?>"/>
 				<?php } else { ?>
-					<img class="img-circle" width="70" alt="No Profile Picture" src="images/misc/default-gravatar-pic.png"/>
+					<img width="100%" alt="No Profile Picture" src="images/misc/default-gravatar-pic.png"/>
 				<?php } ?>
 				&nbsp;
 				<div class="caption center">
