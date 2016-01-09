4bbcb399ff0394d65b7e958572a882ee1a533d90
Amir Azimi <hazz.azimi@gmail.com>
2016-01-09 21:17:15 +0000

    amazon add remove

--- public_html/_/components/php/aside-ad.php
+++ public_html/_/components/php/aside-ad.php
@@ -8,7 +8,4 @@
 	     data-ad-format="auto"></ins>
 	<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 	<!-- end of ads by google -->
-	<!-- ads by amazon -->
-	<iframe src="http://rcm-eu.amazon-adsystem.com/e/cm?t=parsclick-21&o=2&p=42&l=ur1&category=ce&banner=133QTD351PTJW2MR1YR2&f=ifr" class="center" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
-	<!-- end of ads by amazon -->
 </aside>
