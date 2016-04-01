454f6dd8f913e302e953ce23c3505b71e9292407
Amir Azimi <hazz.azimi@gmail.com>
2016-04-01 23:52:42 +0100

    Improved initialize bu creating init()

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,41 +1,46 @@
 <?php
 defined('DS') ? NULL : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? NULL : define("SITE_ROOT", dirname(dirname(__FILE__)));
-//
-// TODO: Change this after getting a domain if needed and possibly adding http:// before it
-//defined('DOMAIN') ? NULL : define('DOMAIN', $_SERVER['HTTP_HOST']);
+/**
+ * defined('DOMAIN') ? NULL : define('DOMAIN', $_SERVER['HTTP_HOST']);
+ */
 defined('DOMAIN') ? NULL : define('DOMAIN', 'www.parsclick.net');
-//
+/**
+ * Paths for public and private
+ */
 defined('LIB_PATH') ? NULL : define('LIB_PATH', SITE_ROOT . DS . 'includes');
 defined('PUB_PATH') ? NULL : define('PUB_PATH', SITE_ROOT . DS . 'public_html');
-//
+/**
+ * Allowable HTML tags to use in articles
+ */
 defined('ARTICLE_ALLOWABLE_TAGS') ? NULL : define('ARTICLE_ALLOWABLE_TAGS', "<h1><h2><h3><h4><h5><h6><strong><em><i><p><code><pre><mark><span><ul><ol><li><dl><dt><dd><a><img><iframe><video><audio>");
-//
-// Email Details
+/**
+ * Email Details
+ */
 defined('ADMIN_EMAIL') ? NULL : define('ADMIN_EMAIL', "info@parsclick.net");
-// TODO: Change based on email configuration
 defined('SMTP') ? NULL : define('SMTP', "n1plcpnl0045.prod.ams1.secureserver.net");
 defined('PORT') ? NULL : define('PORT', 587);
 defined('EMAILUSER') ? NULL : define('EMAILUSER', "do-not-reply@parsclick.net");
 defined('EMAILPASS') ? NULL : define('EMAILPASS', "1365@1986Ha");
 defined('TLS') ? NULL : define('TLS', "tls");
-//
-// Stripe keys
-// TODO: Change based on stripe live keys
+/**
+ * Stripe keys
+ */
 defined('SECRETKEY') ? NULL : define('SECRETKEY', "sk_live_4VsTFfNoXVuTpc4P0X3oZO3E");
 defined('PUBLICKEY') ? NULL : define('PUBLICKEY', "pk_live_4VsTSyMBU0owS4GoclFSt1vk");
-//
-// TODO: Change based on YouTube API key
-// YouTube API key: AIzaSyBHTFWKKWvYfxs9rP0fEgLlPo8K2V1MsoM
+/**
+ * YouTube API key: AIzaSyBHTFWKKWvYfxs9rP0fEgLlPo8K2V1MsoM
+ */
 defined('YOUTUBEAPI') ? NULL : define('YOUTUBEAPI', "AIzaSyBHTFWKKWvYfxs9rP0fEgLlPo8K2V1MsoM");
 defined('MAXRESULTS') ? NULL : define('MAXRESULTS', "50");
-//
-// TODO: Change based on Google reCaptcha API key
-// Register API keys at https://www.google.com/recaptcha/admin
+/**
+ * Register API keys at https://www.google.com/recaptcha/admin
+ */
 defined('RECAPTCHASITEKEY') ? NULL : define('RECAPTCHASITEKEY', "6Leb2fYSAAAAAFp7bkpkNmvuvaOA9phcJN7LoQ7J");
 defined('RECAPTCHASECRETKEY') ? NULL : define('RECAPTCHASECRETKEY', "6Leb2fYSAAAAABU1lHHAc0PcRLRKcs9StJxFXQPE");
-//
-// YouTube and Udemy
+/**
+ * YouTube and Udemy links
+ */
 defined('YOUTUBE') ? NULL : define('YOUTUBE', "https://www.youtube.com/user/PersianComputers/");
 defined('UDEMY') ? NULL : define('UDEMY', "https://www.udemy.com/u/amirhassanazimi/");
 /**
@@ -50,7 +55,9 @@ function init($files = [])
 	}
 }
 
-// Load libraries using the related function
+/**
+ * Load libraries using the related function
+ */
 init([
 	'vendor' . DS . 'autoload.php',
 	'config.php',
@@ -73,7 +80,9 @@ init([
 	'ArticleComment.php',
 	'FailedLogins.php',
 ]);
+//
 $session = new Session();
 $message = $session->message();
+//
 $database = new MySQLDatabase();
 $db       =& $database;
