e59ade748edabfb7128530a26da336e07a951bc9
Amir Azimi <hazz.azimi@gmail.com>
2016-04-08 15:27:16 +0100

    Adds member-forum-info.php

---
+++ includes/layouts/member-forum-info.php
@@ -0,0 +1,30 @@
+<p><em class="text-success">برای آنکه نظر، سوال و دیدکاهتان را مطرح کنید، لطفا به این نکات توجه کنید.</em></p>
+<p class="alert alert-info"><strong class="lead">توجه:</strong> بزودی <code><a href="https://disqus.com/" title="DISQUS" target="_blank"><mark>DISQUS</mark></a></code> جایگذین این انجمن خواهد شد.</p>
+<p class="lead text-danger"><i class="fa fa-info-circle"></i> نکات مهم:</p>
+<ul>
+	<li>از تگ های <code>&lt;strong&gt;&lt;em&gt;&lt;i&gt;&lt;p&gt;&lt;code&gt;&lt;pre&gt;&lt;mark&gt;&lt;span&gt;&lt;ul&gt;&lt;ol&gt;&lt;li&gt;&lt;a&gt;&lt;img&gt;</code>
+	    می توانید استفاده کنید.
+	</li>
+	<li>برای چپ چین کردن از سی اس اس کلاس <code>edit</code> استفاده کنید.</li>
+	<li>برای انداختن کلمات انگلیسی بین کلمات فارسی از تگ <code>&lt;code&gt;</code> استفاده کنید.</li>
+	<li>دور کد های چند خطه از تگ <code>&lt;pre&gt;</code> استفاده کنید</li>
+	<li>اگر لیست اضافه می کنید از تگ های زیر استفاده کنید:
+						<pre class="line-numbers"><code class="language-html">&lt;ul&gt;<br>&lt;li&gt;لیست اول&lt;/li&gt;
+&lt;li&gt;لیست دوم&lt;/li&gt;&lt;/ul&gt;</code></pre>
+	</li>
+	<li>تگ های <code>HTML</code> قابل رویت نیست یعنی وقتی مطلبی رو می سازید که می خواهید کدهای
+		<code>HTML</code> رو نشون بدید این کدها دیده نخواهند شد چون کدهای <code>HTML</code> تبدیل میشوند. برای
+	    نشون دادن این کدها باید <code>&lt;&gt;</code> ها را به این صورت تبدیل کنید:
+								<pre class="line-numbers"><code class="language-html">&lt;html&gt; => &amp;lt;html&amp;gt;
+&lt;/html&gt; => &amp;lt;/html&amp;gt;</code></pre>
+	</li>
+	<li>برای رنگ گرفتن کد های شما ما از <a href="http://prismjs.com/" title="prism" target="_blank">prism
+		</a> استفاده میکنیم. استفاده از <code>prism</code> اینطور هست که کدهاتون رو داخل تگ <code>pre</code> و
+	    بعد داخل تگ <code>code</code> قرار میدهید و به تگ کد کلاس <code>language-{html}</code> میدهید و به تگ
+		<code>pre</code> کلاس <code>line-numbers</code> می دهید. توجه داشته باشید که به جای <code>{html}</code>
+	    می تونید از هر زبانی دیگر استفاده کنید. مثال زیر مثالی هست که معمولا ما از آن استفاده می کنیم
+								<pre class="line-numbers"><code class="language-html">&lt;pre class="line-numbers"&gt;&lt;code
+class="language-php"&gt; کدهای شما &lt;/code&gt;&lt;/pre&gt;</code></pre>
+	</li>
+	<li>سوالات مربوط به هر درس را در انجمن مخصوص به همون درس مطرح کنید. اگر سوال نامرتبط به درس یا سوال مرطبت به درس دیگری داخل درس نامرتبط مطرح کنید، سوال جواب داده نخواهد شد.</li>
+</ul>
--- public_html/forum.php
+++ public_html/forum.php
@@ -8,47 +8,49 @@ $member->check_status();
 find_selected_course(TRUE);
 $errors = '';
 $body   = '';
-if( ! $current_course || ! $current_category) {
-	$current_course = $current_category = Course::find_newest_course();
-}
 ?>
 <?php include_layout_template('header.php'); ?>
 <?php include_layout_template('member_nav.php'); ?>
 <?php echo output_message($message, $errors); ?>
 	<section class="main col-sm-12 col-md-8 col-lg-8">
 		<article id="comments">
-			<h3><i class="fa fa-comments fa-lg"></i> انجمن
-				<a href="member-courses?category=<?php echo urlencode($current_course->category_id); ?>&course=<?php echo urlencode($current_course->id); ?>"
-				   data-toggle="tooltip" title="برگردید به درس"><?php echo htmlentities($current_course->name); ?></a>
-			</h3>
-			<fieldset>
-				<legend></legend>
-				<form class="form-horizontal submit-comment" action="add-comment.php" method="POST" role="form">
-					<!--content-->
-					<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label" for="content">
-						<img class="img-circle pull-right hidden-sm" width="100" src="//www.gravatar.com/avatar/<?php echo md5($member->email); ?>?s=100&d=<?php echo '//' . DOMAIN . '/images/misc/default-gravatar-pic.png'; ?>" alt="<?php echo $member->username; ?>">
-					</label>
-					<input type="hidden" name="course" value="<?php echo urlencode($current_course->id); ?>">
-					<div class="controls">
+			<?php if( ! $current_course && ! $current_category): ?>
+				<h1><i class="fa fa-comments fa-lg"></i> انجمن، سوالات و نظرات</h1>
+				<?php include_layout_template('member-forum-info.php'); ?>
+			<?php else: ?>
+				<h3><i class="fa fa-comments fa-lg"></i> انجمن
+					<a href="member-courses?category=<?php echo urlencode($current_course->category_id); ?>&course=<?php echo urlencode($current_course->id); ?>"
+					   data-toggle="tooltip" title="برگردید به درس"><?php echo htmlentities($current_course->name); ?></a>
+				</h3>
+				<fieldset>
+					<legend></legend>
+					<form class="form-horizontal submit-comment" action="add-comment.php" method="POST" role="form">
+						<!--content-->
+						<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label" for="content">
+							<img class="img-circle pull-right hidden-sm" width="100" src="//www.gravatar.com/avatar/<?php echo md5($member->email); ?>?s=100&d=<?php echo '//' . DOMAIN . '/images/misc/default-gravatar-pic.png'; ?>" alt="<?php echo $member->username; ?>">
+						</label>
+						<input type="hidden" name="course" value="<?php echo urlencode($current_course->id); ?>">
+						<div class="controls">
 							<textarea class="col-xs-12 col-sm-10 col-md-10 col-lg-10" name="body" id="body" rows="3" required
 							          placeholder="سوال یا نظرتان را اینجا وارد کنید و کد ها را داخل تگ <pre> وارد کنید
 برای چپ چین کردن از <'p class='edit> استفاده کنید
 کلمات انگلیسی بین کلمات فارسی را داخل تگ <code> بیاندازید"></textarea>
+						</div>
+						<!--buttons-->
+						<div class="controls col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
+							<button class="btn btn-primary" name="submit" id="submit" type="submit">
+								بفرست
+							</button>
+						</div>
+					</form>
+				</fieldset>
+				<hr/>
+				<div id="forum">
+					<div id="ajax-comments">
+						<?php include_layout_template('course-comments.php'); ?>
 					</div>
-					<!--buttons-->
-					<div class="controls col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
-						<button class="btn btn-primary" name="submit" id="submit" type="submit">
-							بفرست
-						</button>
-					</div>
-				</form>
-			</fieldset>
-			<hr/>
-			<div id="forum">
-				<div id="ajax-comments">
-					<?php include_layout_template('course-comments.php'); ?>
 				</div>
-			</div>
+			<?php endif; ?>
 		</article>
 	</section>
 	<section class="sidebar col-sm-12 col-md-4 col-lg-4">
