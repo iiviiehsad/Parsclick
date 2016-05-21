5a3cf79ea33d83c9e9ed5b3f19767a1adbff16cd
Amir Azimi <hazz.azimi@gmail.com>
2016-05-21 16:54:46 +0100

    Updates Laravel like comments

--- github.php
+++ github.php
@@ -1,11 +1,15 @@
 <?php
 
-/**
- * This file is accessed by `github.php` in public directory
- *
- * Shell Script:
- * 1. Go back one directory
- * 2. Use `git pull` to pull from GitHub
+/*
+ |----------------------------------------------------------------
+ | This file is accessed by `github.php` in public directory
+ |----------------------------------------------------------------
+ |
+ | Shell Script:
+ | 1. Go back one directory
+ | 2. Use `git pull` to pull from GitHub
+ |
  */
+
 
 return `cd ..; git pull`;
