c5fe69c0075cddb4ea58e3d7b5f62d3935ec0e23
Amir Azimi <hazz.azimi@gmail.com>
2016-05-21 17:00:03 +0100

    Updates Laravel like comments

--- github.php
+++ github.php
@@ -5,7 +5,8 @@
  | This file is accessed by `github.php` in public directory
  |----------------------------------------------------------------
  |
- | Shell Script:
+ | shell_exec() function can also be used instead of back ticks
+ |
  | 1. Go back one directory
  | 2. Use `git pull` to pull from GitHub
  |
--- public_html/github.php
+++ public_html/github.php
@@ -5,12 +5,13 @@
  | This file will call `github.php` in the parent directory
  |----------------------------------------------------------------
  |
- | We use two version of `github.php` file
- | because GitHub can only access this file in our host
+ | We use two version of `github.php` file because
+ | GitHub can only access this file in our host
  |
  | Use one of the code line below only:
- | The First one will use PHP to access the file
- | The Second one will use Shell PHP to access the file
+ | The First one is using PHP to access the file
+ | The Second one is using Shell PHP to access the file
+ |
  */
 
 return include __DIR__ . '/../github.php';
