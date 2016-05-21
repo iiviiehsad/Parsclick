658f7e14a0b1035be6813e57b8c619174b9ed951
Amir Azimi <hazz.azimi@gmail.com>
2016-05-21 03:14:39 +0100

    Ready github.php

---
+++ github.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * This file is accessed by `github.php` in public directory
+ *
+ * Shell Script:
+ * 1. Go back one directory
+ * 2. Use `git pull` to pull from GitHub
+ */
+
+return `cd ..; git pull`;
--- public_html/github.php
+++ public_html/github.php
@@ -1,1 +1,13 @@
-<?php `git pull`;
+<?php
+
+/**
+ * We use two version of `github.php` file
+ * because GitHub can only access this file in our host
+ *
+ * Use one of the code line below only:
+ * The First one will use PHP to access the file
+ * The Second one will use Shell PHP to access the file
+ */
+
+return include __DIR__ . '/../github.php';
+// return `php -f ../github.php`;
