8eeebad77fc3d79f105a07d0251b134404b61ab2
Amir Azimi <hazz.azimi@gmail.com>
2017-04-23 01:52:43 +0100

    Add ads

--- includes/layouts/aside-ad.php
+++ includes/layouts/aside-ad.php
@@ -1,10 +1,11 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle"
      style="display:block"
-     data-ad-format="autorelaxed"
      data-ad-client="ca-pub-7301263638062324"
-     data-ad-slot="6314959353"></ins>
-<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-
+     data-ad-slot="4941652953"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 <!--style="display:block;margin:10px auto;"-->
 <!--center-->
